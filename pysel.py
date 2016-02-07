3a181f809d2337794ed2f5cb78c090c7436264ec
Caio Ariede <caio.ariede@gmail.com>
2016-02-07 08:13:39 -0200

    Wrap match logic in a class

--- pysel.py
+++ pysel.py
@@ -23,6 +23,11 @@ class Selector(object):
     def __repr__(self):
         return 'Selector <{}>'.format(self.name)
 
+    @staticmethod
+    def parse(string):
+        for sel in string.split(','):
+            yield Selector.build(sel.strip())
+
     @classmethod
     def build(cls, sel, _direct=False):
         subs = regex.split(SUBSEL_RE, sel, 1)
@@ -54,9 +59,98 @@ class Selector(object):
                    _next=_next, _direct=_direct)
 
 
-def sel(selector):
-    for sel in selector.split(','):
-        yield Selector.build(sel.strip())
+class MatchEngine(object):
+    pseudo_fns = {}
+
+    def __init__(self):
+        self.register_pseudo('not', self.pseudo_not)
+
+    def register_pseudo(self, name, fn):
+        self.pseudo_fns[name] = fn
+
+    @staticmethod
+    def pseudo_not(matcher, node, value):
+        return not matcher.match_node(Selector.build(value), node)
+
+    def match(self, selector, data):
+        selectors = Selector.parse(selector)
+        for selector in selectors:
+            yield from self.match_data(selector, data)
+
+    def match_data(self, selector, data):
+        for node, body in self._iter_data(data):
+            match = self.match_node(selector, node)
+
+            if match:
+                if selector._next:
+                    if body:
+                        yield from self.match_data(selector._next, body)
+                else:
+                    yield node
+
+            if body and not selector._direct:
+                yield from self.match_data(selector, body)
+
+    def match_node(self, selector, node):
+        match = all(self.match_rules(selector, node))
+
+        if match and selector.pseudos:
+            match &= all(self.match_pseudos(selector, node))
+
+        return match
+
+    def match_rules(self, selector, node):
+        if selector.typ:
+            yield self.match_type(selector.typ, node)
+
+        if selector.id_:
+            yield self.match_id(selector.id_, node)
+
+    def match_pseudos(self, sobj, d):
+        for p in sobj.pseudos:
+            yield self.pseudo_fns[p[0]](self, d, p[1])
+
+    def _iter_data(self, data):
+        for tupl in self.iter_data(data):
+            if len(tupl) != 2:
+                raise Exception(
+                    'The iter_data method must yield pair tuples containing '
+                    'the node and its body (empty if not available)')
+            yield tupl
+
+    def match_type(self, typ, node):
+        raise NotImplementedError
+
+    def match_id(self, id_, node):
+        raise NotImplementedError
+
+    def iter_data(self, data):
+        raise NotImplementedError
+
+
+class CustomMatchEngine(MatchEngine):
+    def __init__(self):
+        super().__init__()
+        self.register_pseudo('extends', self.pseudo_extends)
+
+    @staticmethod
+    def pseudo_extends(matcher, node, value):
+        return value in getattr(node, 'extends', [])
+
+    def match_type(self, typ, node):
+        if typ == 'class':
+            cls = self.CLS
+        elif typ == 'def':
+            cls = self.DEF
+
+        return isinstance(node, cls)
+
+    def match_id(self, id_, node):
+        return node.name == id_
+
+    def iter_data(self, data):
+        for node in data:
+            yield node, getattr(node, 'body', None)
 
 
 if __name__ == '__main__':
@@ -64,13 +158,13 @@ if __name__ == '__main__':
 
     class TestSObjs(unittest.TestCase):
         def test_class_selector(self):
-            sobjs = list(sel('.class > def'))
+            sobjs = list(Selector.parse('.class > def'))
 
             self.assertEqual(len(sobjs), 1)
             self.assertEqual(sobjs[0].name, '.class')
 
         def test_class_selector_child(self):
-            sobjs = list(sel('.class > def'))
+            sobjs = list(Selector.parse('.class > def'))
 
             self.assertEqual(len(sobjs), 1)
             self.assertEqual(sobjs[0].name, '.class')
@@ -80,13 +174,13 @@ if __name__ == '__main__':
             self.assertTrue(sobjs[0]._next._direct)
 
         def test_type_selector(self):
-            sobjs = list(sel('class'))
+            sobjs = list(Selector.parse('class'))
 
             self.assertEqual(len(sobjs), 1)
             self.assertEqual(sobjs[0].name, 'class')
 
         def test_composed_selector(self):
-            sobjs = list(sel('class.foo.bar'))
+            sobjs = list(Selector.parse('class.foo.bar'))
 
             self.assertEqual(len(sobjs), 1)
             self.assertIsInstance(sobjs[0], Selector)
@@ -94,7 +188,7 @@ if __name__ == '__main__':
             self.assertEqual(sobjs[0].classes, ['foo', 'bar'])
 
         def test_child_selector(self):
-            sobjs = list(sel('class > def'))
+            sobjs = list(Selector.parse('class > def'))
 
             self.assertEqual(len(sobjs), 1)
             self.assertIsInstance(sobjs[0], Selector)
@@ -105,7 +199,7 @@ if __name__ == '__main__':
             self.assertTrue(sobjs[0]._next._direct)
 
         def test_nonchild_selector(self):
-            sobjs = list(sel('class def'))
+            sobjs = list(Selector.parse('class def'))
 
             self.assertEqual(len(sobjs), 1)
             self.assertEqual(sobjs[0].name, 'class')
@@ -114,7 +208,7 @@ if __name__ == '__main__':
             self.assertEqual(sobjs[0]._next.name, 'def')
 
         def test_pseudos(self):
-            sobjs = list(sel(':not(1)'))
+            sobjs = list(Selector.parse(':not(1)'))
 
             self.assertEqual(len(sobjs), 1)
             self.assertEqual(len(sobjs[0].pseudos), 1)
@@ -122,7 +216,7 @@ if __name__ == '__main__':
             self.assertEqual(sobjs[0].pseudos[0][1], '1')
 
         def test_nested_pseudos(self):
-            sobjs = list(sel(':not(:not(1))'))
+            sobjs = list(Selector.parse(':not(:not(1))'))
 
             self.assertEqual(len(sobjs), 1)
             self.assertEqual(len(sobjs[0].pseudos), 1)
@@ -130,7 +224,7 @@ if __name__ == '__main__':
             self.assertEqual(sobjs[0].pseudos[0][1], ':not(1)')
 
         def test_compound_pseudos(self):
-            sobjs = list(sel(':not(1):not(2)'))
+            sobjs = list(Selector.parse(':not(1):not(2)'))
 
             self.assertEqual(len(sobjs), 1)
             self.assertEqual(len(sobjs[0].pseudos), 2)
@@ -140,61 +234,11 @@ if __name__ == '__main__':
             self.assertEqual(sobjs[0].pseudos[1][1], '2')
 
     # test custom matcher
-    from collections import namedtuple
-
     class TestCustomMatcher(unittest.TestCase):
+        from collections import namedtuple
+
         CLS = namedtuple('ClassDef', ('name', 'extends', 'body'))
         DEF = namedtuple('FunctionDef', ('name',))
-
-        def match(self, selector, data):
-            sobjs = list(sel(selector))
-            result = list(self._match(sobjs, data))
-            return result
-
-        def _match(self, sobjs, data):
-            for sobj in sobjs:
-                yield from self._match_sub(sobj, data)
-
-        def _match_sub(self, sobj, data):
-            for d in data:
-                match = self._match_rules(sobj, d)
-
-                if match:
-                    if sobj._next:
-                        if hasattr(d, 'body'):
-                            yield from self._match_sub(sobj._next, d.body)
-                    else:
-                        yield d
-
-                if hasattr(d, 'body') and not sobj._direct:
-                    yield from self._match_sub(sobj, d.body)
-
-        def _match_rules(self, sobj, d):
-            match = all(self._match_item(sobj, d))
-
-            if match and sobj.pseudos:
-                match &= all(self._match_pseudos(sobj, d))
-
-            return match
-
-        def _match_item(self, sobj, d):
-            if sobj.typ:
-                typcls = {
-                    'class': self.CLS,
-                    'def': self.DEF,
-                }
-
-                yield isinstance(d, typcls[sobj.typ])
-
-            if sobj.id_:
-                yield d.name == sobj.id_
-
-        def _match_pseudos(self, sobj, d):
-            for p in sobj.pseudos:
-                if p[0] == 'not':
-                    yield not self._match_rules(Selector.build(p[1]), d)
-                elif p[0] == 'extends':
-                    yield p[1] in getattr(d, 'extends', [])
 
         def setUp(self):
             self.data = [
@@ -208,27 +252,32 @@ if __name__ == '__main__':
                 self.DEF('baz'),
             ]
 
+            self.matcher = CustomMatchEngine()
+            self.matcher.CLS = self.CLS
+            self.matcher.DEF = self.DEF
+
+            self.match = lambda s: list(self.matcher.match(s, self.data))
+
         def test_typ(self):
-            self.assertEqual(len(self.match('class', self.data)), 2)
-            self.assertEqual(len(self.match('class, def', self.data)), 6)
+            self.assertEqual(len(self.match('class')), 2)
+            self.assertEqual(len(self.match('class, def')), 6)
 
         def test_child(self):
-            self.assertEqual(len(self.match('class > def', self.data)), 3)
+            self.assertEqual(len(self.match('class > def')), 3)
 
         def test_ids(self):
-            self.assertEqual(len(self.match('def#bla', self.data)), 0)
-            self.assertEqual(len(self.match('def#foo', self.data)), 1)
+            self.assertEqual(len(self.match('def#bla')), 0)
+            self.assertEqual(len(self.match('def#foo')), 1)
 
         def test_pseudos(self):
-            return
-            self.assertEqual(len(self.match(':extends(object)', self.data)), 1)
+            self.assertEqual(len(self.match(':extends(object)')), 1)
 
         def test_nested_pseudos(self):
             self.assertEqual(
-                len(self.match(':not(:extends(object))', self.data)), 5)
+                len(self.match(':not(:extends(object))')), 5)
             self.assertEqual(
-                len(self.match(':extends(object):not(def)', self.data)), 1)
+                len(self.match(':extends(object):not(def)')), 1)
             self.assertEqual(
-                len(self.match(':not(def)', self.data)), 2)
+                len(self.match(':not(def)')), 2)
 
     unittest.main()
